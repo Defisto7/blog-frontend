deec0ab256ff7c16926d667ecf797a9541c9f2e5
Dima Yanovskji <86802309+Defisto7@users.noreply.github.com>
2022-07-27 10:08:21 +0300

    Master
    allow only the author to edit the post

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,7 @@ import { fetchPosts, fetchTags } from "../redux/slices/posts";
 export const Home = () => {
   const distatch = useDispatch();
   const { posts, tags } = useSelector((state) => state.posts);
+  const userData = useSelector((state) => state.auth.data);
 
   const isPostsLoading = posts.status === "loading";
   const isTagsLoading = tags.status === "loading";
@@ -47,7 +48,7 @@ export const Home = () => {
                 viewsCount={obj.viewsCount}
                 commentsCount={3}
                 tags={obj.tags}
-                isEditable
+                isEditable={userData?._id === obj.user._id}
               />
             )
           )}
